68d50a5eeb37cdf9aa0d70671aa802abaec28abe
manasisingh26 <120970136+manasisingh26@users.noreply.github.com>
2022-12-31 18:11:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import '../styles/App.css';
 const App = () => {
   const [value,setValue]= useState('square')
@@ -11,20 +11,18 @@ const App = () => {
   return (
     <div id="main">
       <div id="shape-creator">
-    <select onChange={(e)=> {setValue(e.target.value) }}>
+        <select onChange={(e)=> {setValue(e.target.value) }}>
     <option value="square">Square</option>
- <option value="circle">Circle</option>
+<option value="circle">Circle</option>
 </select>
-<button onClick={handleChange}Add shape</button>
-        </div>
+<button onClick={handleChange}>Add shape</button>
+      </div>
       <div id="shapes-holder">
-        {shapeList.map((ele,idx)=>ele === 'square' ? <div className='square'>{idx}
-        </div> : <div className= 'circle'>{idx}</div>)}
+        {shapeList.map((ele,idx)=>ele === 'square' ? <div className='square'>{idx}</div> : <div className= 'circle'>{idx}</div>)}
       </div>
     </div>
   )
 }
-
 
 
 export default App;
